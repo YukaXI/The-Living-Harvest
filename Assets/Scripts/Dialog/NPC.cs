4526c6fa5a917bfc91d4e7de4b5f599b64979737
YukaXI <liuh9646@gmail.com>
2026-08-13 20:48:38 +0200

    +NPC Script
    +Weiter am NPC Script gearbeitet

--- Assets/Scripts/Dialog/NPC.cs
+++ Assets/Scripts/Dialog/NPC.cs
@@ -1,16 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Collections;
 
 public class NPC : MonoBehaviour, IInteractable
 {
-public NPCDialogue dialogueData;
-public GameObject dialoguePanel;
-public TMP_Text dialogueText, nameText;
-public Image portraitImage;
+   public NPCDialogue dialogueData;
+   public GameObject dialoguePanel;
+   public TMP_Text dialogueText, nameText;
+   public Image portraitImage;
 
-private int dialogueIndex;
-private bool isTyping, isDialogueActive;
+   private int dialogueIndex;
+   private bool isTyping, isDialogueActive;
 
 public bool CanInteract()
 {
@@ -22,5 +24,66 @@ public void Interact()
 {
     if (dialogueData == null || (PauseController.IsGamePaused && !isDialogueActive))
         return;
+    
+    if(isDialogueActive)
+    {
+       //NextLine
+    }
+    else
+    {
+        StartDialogue();
+    }
 }
+
+void StartDialogue()
+{
+    isDialogueActive = true;
+    dialogueIndex = 0;
+    
+    nameText.SetText(dialogueData.name);
+    portraitImage.sprite = dialogueData.npcPortrait;
+    
+    dialoguePanel.SetActive(true);
+    PauseController.SetPause(false);
+
+    StartCoroutine(TypeLine());
+}
+
+void NextLine()
+{
+    if (isTyping)
+    {  
+        StopAllCoroutines();
+        dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+        isTyping = false;
+    }
+    else if(++dialogueIndex < dialogueData.dialogueLines.Length)
+    {
+        StartCoroutine(TypeLine());
+    }
+    else
+    {
+        //EndDialogue
+    }
+}
+
+ IEnumerator TypeLine()
+{
+    isTyping = true;
+    dialogueText.SetText("");
+    
+    foreach(char letter in dialogueData.dialogueLines[dialogueIndex])
+    {
+        dialogueText.text += letter;
+        yield return new WaitForSeconds(dialogueData.typingSpeed);
+    }
+
+    isTyping = false;
+
+    if (dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+    {
+       yield return new WaitForSeconds(dialogueData.autoProgressDelay); 
+    }
+}
+    
 }
