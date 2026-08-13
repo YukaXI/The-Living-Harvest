9dba72ea75e03303a1dacce1f72d93b8c82e031b
Dmdu44 <robin.veh@gmx.de>
2026-08-14 01:33:47 +0200

    Enemy Push 2 Health Hinzugefügt
    + Healthbar und script für den MC
    + Veränderung in PlayerMovement + InputPlayer Script
    + Enemy Health Beginn
    + Scene nur für Robin

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,24 +1,59 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Project.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
+        #region Hash
+        
+        private static readonly int HashActionTrigger = Animator.StringToHash("ActionTrigger");
+        
+        private static readonly int HashActionId = Animator.StringToHash("ActionId");
+        
+        #endregion
+
+        public Transform attackPoint;
+        public float weaponRange = 1;
+        public LayerMask enemyLayer;
+        public int damage = 1;
+        
         [SerializeField]
         private PlayerInputManager _playerInputManager;
         
         [SerializeField]
+        private Animator anim;
+        
+        [SerializeField]
         private Rigidbody2D _rigidbody;
-
         [SerializeField]
         private float _baseSpeed = 5f;
-
+        [SerializeField]
+        private float cooldown = 2;
+        [SerializeField] 
+        private float timer;
+        
+        
+        
         public Rigidbody2D Rigidbody => _rigidbody;
         public Vector2 CurrentMovementDirection { get; private set; }
+
+        private void Awake()
+        {
+            anim = GetComponentInChildren<Animator>();
+        }
         
         private void FixedUpdate()
         {
             Move(Time.fixedDeltaTime);
+        }
+
+        private void Update()
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
         }
 
         private void Move(float deltaTime)
@@ -27,5 +62,29 @@ namespace Project.Player
 
             _rigidbody.linearVelocity = CurrentMovementDirection * _baseSpeed;
         }
+        
+        public void Attack(InputAction.CallbackContext context)
+        {
+            if (timer <= 0)
+            {
+                timer = cooldown;
+                AnimationSetActionId(1);
+
+                Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, enemyLayer);
+
+            }
+        }
+
+        public void AttackEnde(InputAction.CallbackContext ctx)
+        {
+            anim.SetBool("isAttacking", false);
+        }
+        
+        private void AnimationSetActionId(int id)
+        {
+            anim.SetTrigger(HashActionTrigger); 
+            anim.SetInteger(HashActionId, id);
+        }
+
     }
 }
