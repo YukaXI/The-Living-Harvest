ad18ee8426813e42556c34d05c9a8397048d088a
YukaXI <liuh9646@gmail.com>
2026-08-13 13:44:41 +0200

    +NPC Script
    +NPC Script
    +NPC Ordner in Assets

--- Assets/Scripts/Dialog/NPCDialogue.cs
+++ Assets/Scripts/Dialog/NPCDialogue.cs
@@ -6,9 +6,10 @@ public class NPCDialogue : ScriptableObject
     public string npcName;
     public Sprite npcPortrait;
     public string[] dialogueLines;
+    public bool[] autoProgressLines;
+    public float autoProgressDelay = 1.5f;
     public float typingSpeed = 0.05f;
     public AudioClip voiceSound;
     public float voicePitch = 1f;
-    public bool[] autoProgressLines;
-    public float autoProgressDelay = 1.5f;
+
 }
