d1f36a7916bb1bc261cf7b23d2b118de7ed15d34
Dmdu44 <robin.veh@gmx.de>
2026-08-18 10:47:55 +0200

    Versuch Enemy zum laufen zu bringen

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -3,20 +3,27 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    private static readonly int BackAnimationHashTrigger = Animator.StringToHash("BackTrigger");
+    private static readonly int FrontAnimationHashTrigger = Animator.StringToHash("FrontTrigger");
+    private static readonly int SideAnimationHashTrigger = Animator.StringToHash("SideTrigger");
+    
+    
     [SerializeField] private Rigidbody2D rb;
 
     [SerializeField] private float speed;
+
+    private Animator _anim; 
     
     private bool isChasing;
 
-    public Transform target;
+    private int facingDirectionX = -1;
     
-    public Vector2 CurrentMovementDirection { get; private set; }
-
+    public Transform target;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -33,16 +40,45 @@ public class EnemyMovement : MonoBehaviour
 
     private void MovementState()
     {
-       
-        
         Vector2 direction = (target.position - transform.position).normalized;
+        
+        if (target.position.x > transform.position.x && facingDirectionX == -1 || target.position.x < transform.position.x && facingDirectionX == 1)
+        {
+            SideAnimation();
+        }
+
+        if (target.position.y > transform.position.y)
+        {
+            FrontAnimation();
+        }
+        
+        else if (target.position.y > transform.position.y)
+        {
+            BackAnimation();
+        }
+        
         rb.linearVelocity = direction;
     }
 
+    private void SideAnimation()
+    {
+        facingDirectionX *= -1;
+        _anim.SetTrigger(SideAnimationHashTrigger);
+        transform.localScale = new Vector3(-transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void FrontAnimation()
+    {
+        _anim.SetTrigger(FrontAnimationHashTrigger);
+        transform.localScale = new Vector3(transform.localScale.y * -1, transform.localScale.x, transform.localScale.z);
+    }
+
+    private void BackAnimation()
+    {
+        _anim.SetTrigger(BackAnimationHashTrigger);
+        transform.localScale = new Vector3(-transform.localScale.y * 1, transform.localScale.x, transform.localScale.z);
+    }
     
-
-
-
     public enum EnemyState
     {
         Knockback,
