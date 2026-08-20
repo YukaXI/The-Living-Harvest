36b914c4dea0998d0608accf1e004a742dce79e2
YukaXI <liuh9646@gmail.com>
2026-08-20 12:47:44 +0200

    +PauseMenuControllerScript
    +PauseMenuControllerScript
    +Buttons eingerichtet

---
+++ Assets/Scripts/ButtonManager/PauseMenuController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseMenu.activeSelf)
+                {
+                Resume();
+                }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    
+    public void Resume()
+    {
+     PauseMenu.SetActive(false);
+     Time.timeScale = 1;
+    }  
+    
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    
+    public void Exit()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
